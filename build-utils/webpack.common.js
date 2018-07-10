0453d42c3785b0a8a6ef23333931a838bdf1805d
sujinleeme <sujinlee.me@gmail.com>
2018-07-10 14:08:26 +0900

    chore: Change bable preset and add react in webpack output vendors
    see: #5

---
+++ build-utils/webpack.common.js
@@ -0,0 +1,49 @@
+const commonPaths = require("./common-paths")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+const config = {
+	entry: {
+		vendor: ["@material-ui/core", "babel-polyfill", "react", "react-dom", "redux"]
+	},
+	output: {
+		path: commonPaths.outputPath,
+		publicPath: "/"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						plugins: [
+							"babel-plugin-redux-saga"
+						]
+					}
+				}
+			}
+		]
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					chunks: "initial",
+					test: "vendor",
+					name: "vendor",
+					enforce: true
+				}
+			}
+		}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: `public/index.html`,
+			favicon: `public/favicon.ico`
+		})
+	]
+}
+
+module.exports = config
---
+++ build-utils/webpack.prod.js
@@ -0,0 +1,59 @@
+const commonPaths = require("./common-paths")
+
+const webpack = require("webpack")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+const config = {
+	mode: "production",
+	entry: {
+		app: ["babel-polyfill", `${commonPaths.appEntry}/index.js`]
+	},
+	output: {
+		filename: "static/[name].[hash].js"
+	},
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: [
+						{
+							loader: "css-loader",
+							options: {
+								modules: true,
+								importLoaders: 1,
+								camelCase: true,
+								sourceMap: true
+							}
+						},
+						{
+							loader: "postcss-loader",
+							options: {
+								config: {
+									ctx: {
+										autoprefixer: {
+											browsers: "last 2 versions"
+										}
+									}
+								}
+							}
+						}
+					]
+				})
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin({
+			filename: "styles/styles.[hash].css",
+			allChunks: true
+		}),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		})
+	]
+}
+
+module.exports = config
