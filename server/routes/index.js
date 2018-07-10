ce38e876588dd4ac59d449c4f202904b0fdb6069
sujinleeme <sujinlee.me@gmail.com>
2018-07-10 14:04:54 +0900

    chore: Apply package-lock.json to .gitignore

--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const user = require("./user")
-const inputAudio = require("./inputAudio")
-
-module.exports = (router) => {
-	user(router)
-	inputAudio(router)
-}
--- src/components/Loading.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import PropTypes from "prop-types"
-import LinearProgress from "@material-ui/core/LinearProgress"
-import ReactDelayRender from "react-delay-render"
-
-const Loading = () => <LinearProgress />
-
-export default ReactDelayRender({delay: 300})(Loading)
