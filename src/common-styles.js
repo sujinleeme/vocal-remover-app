ae1fbe2958155310d08bcf692a76d7ab48991214
sujinleeme <sujinlee.me@gmail.com>
2018-07-11 04:22:45 +0900

    feat: Create align styles object

---
+++ src/common-styles.js
@@ -0,0 +1,14 @@
+export const alignStyle = {
+	center: {
+		justifyContent: "center",
+		flexDirection: "column",
+		TextAlign: "center",
+		display: "flex",
+		alignItems: "center"
+	},
+
+	right: {
+		display: "flex",
+		justifyContent: "flex-end"
+	}
+}
