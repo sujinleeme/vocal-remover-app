d201ae7f4f906003fb34e91f95894496a6ce5372
sujinleeme <sujinlee.me@gmail.com>
2018-07-20 17:43:43 +0900

    feat: Open signin modal when user doesn't login yet
    see: #15

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -5,6 +5,8 @@ import classNames from "classnames"
 import { withStyles } from "@material-ui/core/styles"
 import UserProfile from "./MyPage/UserProfile"
 import { connect } from "react-redux"
+import { modalRequest } from "../modal/actions"
+import { SIGN_IN_MODAL } from "../modal/constants"
 
 const styles = {
 	row: {
@@ -21,20 +23,29 @@ const styles = {
 }
 
 
-const MyPage = ({user}) => {
-	return (
-		<div>
-			<Layout>
-				<UserProfile
-					user={user}
-				/>
-			</Layout>
-		</div>
-	)
+class MyPage extends React.Component {
+	componentDidMount () {
+    const { user } = this.props
+		if (!user) {
+    	return this.props.modalRequest({modalOpen: true, modalType: SIGN_IN_MODAL})
+		}
+	}
+	
+  render() {
+  	const { user } = this.props
+    return (
+			<div>
+				<Layout>
+          { user &&	<UserProfile user={user}/>}
+				</Layout>
+			</div>
+    );
+  }
 }
+
 
 const mapStateToProps = state => ({
 	user: state.client.user
 })
 
-export default withStyles(styles)(connect(mapStateToProps, null)(MyPage))
+export default withStyles(styles)(connect(mapStateToProps, {modalRequest})(MyPage))
