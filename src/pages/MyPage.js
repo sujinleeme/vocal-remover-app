97b62b978fc9724d7053823e5c327b65f3fa0ae0
sujinleeme <sujinlee.me@gmail.com>
2018-07-22 16:48:04 +0900

    refactor: Delete unused withStyles

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,27 +1,9 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import Layout from "../components/Layout"
-import classNames from "classnames"
-import { withStyles } from "@material-ui/core/styles"
 import UserProfile from "./MyPage/UserProfile"
 import { connect } from "react-redux"
 import { modalRequest } from "../modal/actions"
 import { SIGN_IN_MODAL } from "../modal/constants"
-
-const styles = {
-	row: {
-		display: "flex",
-		justifyContent: "center"
-	},
-	avatar: {
-		margin: 10
-	},
-	bigAvatar: {
-		width: 60,
-		height: 60
-	}
-}
-
 
 class MyPage extends React.Component {
 	componentDidMount () {
@@ -48,4 +30,4 @@ const mapStateToProps = state => ({
 	user: state.client.user
 })
 
-export default withStyles(styles)(connect(mapStateToProps, {modalRequest})(MyPage))
+export default connect(mapStateToProps, {modalRequest})(MyPage)
