95a1070048f8363d53c1e3451ef3e50edbbefc23
sujinleeme <sujinlee.me@gmail.com>
2018-07-23 03:15:34 +0900

    refactor: Apply Styling

--- src/components/AudioFileDropZone.js
+++ src/components/AudioFileDropZone.js
@@ -1,39 +1,76 @@
 import React from "react"
 import Dropzone from "react-dropzone"
+import { Paper, withStyles, Typography, Button, Icon } from "@material-ui/core"
+import { connect } from "react-redux"
+
+const styles = theme => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    width: "100%",
+    padding: theme.spacing.unit * 2,
+    cursor: "pointer"
+  },
+  contents: {
+    padding: theme.spacing.unit * 4,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column"
+  },
+  icon: {
+    fontSize: "80px",
+    color: "#ccc",
+    margin: theme.spacing.unit * 4
+  }
+})
 
 class AudioFileDropZone extends React.Component {
-	constructor() {
-		super()
-		this.state = {files: []}
-	}
-	
-	onDrop(files) {
-		this.setState({
-			files
-		})
-	}
-	
-	render() {
-		return (
-			<section>
-				<div className="dropzone">
-					<Dropzone
-						accept="audio/*"
-						onDrop={ this.onDrop.bind(this) }>
-						<p>Try dropping some files here, or click to select files to upload.</p>
-					</Dropzone>
-				</div>
-				<aside>
-					<h2>Dropped files</h2>
-					<ul>
-						{
-							this.state.files.map(f => <li key={ f.name }>{ f.name } - { f.size } bytes</li>)
-						}
-					</ul>
-				</aside>
-			</section>
-		)
-	}
+  constructor() {
+    super()
+    this.state = {files: []}
+  }
+  
+  onDrop(files) {
+    this.setState({
+      files
+    })
+  }
+  
+  render() {
+    const {classes} = this.props
+    return (
+      <section className={ classes.root }>
+        <Dropzone accept="audio/*"
+                  style={ {
+                    width: "100%"
+                  } }
+                  onDrop={ this.onDrop.bind(this) }>
+          <div className={ classes.contents }>
+            <Typography variant="title"
+                        align="center"
+                        gutterBottom>Drag & drop an audio file to import, or browse.</Typography>
+            <Icon className={ classes.icon }>library_music</Icon>
+            <Typography variant="body1"
+                        align="center"
+                        gutterBottom>
+              AIFF, WAVE(WAV),
+              FLAC, ALAC, OGG, MP2, MP3, AAC, AMR, and WMA (Max 5GB)
+            </Typography>
+            <aside>
+              <ul>
+                {
+                  this.state.files.map(f => <li key={ f.name }>{ f.name } - { f.size } bytes</li>)
+                }
+              </ul>
+            </aside>
+          </div>
+        </Dropzone>
+      </section>
+    )
+  }
 }
 
-export default AudioFileDropZone
+export default withStyles(styles)(connect(null, {})(AudioFileDropZone))
--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -1,19 +1,34 @@
 import React from "react"
 import Layout from "../components/Layout"
 import AudioFileDropZone from "../components/AudioFileDropZone"
-import Waveform from '../components/Waveform'
+import Player from "../player"
+import { Paper, withStyles } from "@material-ui/core"
 
-const UploadPage = () => {
-	return (
-		<div>
-			<Layout>
-				<AudioFileDropZone />
-			</Layout>
-			<Waveform
-				audioFile="https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3"
-			/>
-		</div>
-	)
+const styles = theme => ({
+  row: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    width: "100%",
+  }
+})
+
+const UploadPage = ({classes}) => {
+  return (
+    <div>
+      <Layout>
+        <Paper className={classes.row}>
+          <AudioFileDropZone/>
+        </Paper>
+      </Layout>
+      <Layout>
+        <Player
+          audioFile="https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3"
+        />
+      </Layout>
+    </div>
+  )
 }
 
-export default UploadPage
+export default withStyles(styles)(UploadPage)
