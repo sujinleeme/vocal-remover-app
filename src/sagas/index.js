d5bb5710210e4b49585bec65824b9d2a877a6d65
sujinleeme <sujinlee.me@gmail.com>
2018-07-24 17:00:58 +0900

    Audio file upload with redux-saga
    see: #17

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,12 +2,13 @@ import { all } from "redux-saga/effects"
 import ModalSaga from "../modal/sagas"
 import SignupSaga from "../signup/sagas"
 import SnackbarSaga from "../snackbar/sagas"
+import UploaderSaga from "../upload/sagas"
 
 export default function* rootSagas() {
-	
 	yield all([
 		ModalSaga(),
 		SignupSaga(),
 		SnackbarSaga(),
+    UploaderSaga()
 	])
 }
---
+++ src/upload/reducer.js
@@ -0,0 +1,73 @@
+import { UPLOAD_RESET, UPLOAD_REQUEST, UPLOAD_SUCCESS, UPLOAD_ERROR, UPLOAD_PROGRESS } from "./constants"
+
+const initialState = {
+  file: null,
+  requesting: false,
+  successful: false,
+  progress: 0,
+  messages: [],
+  errors: []
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case UPLOAD_RESET:
+      return {
+        file: null,
+        requesting: false,
+        successful: false,
+        progress: 0,
+        messages: [],
+        errors: []
+      }
+    case UPLOAD_REQUEST:
+      return {
+        file: null,
+        requesting: true,
+        successful: false,
+        progress: 0,
+        messages: [{body: "Uploading...", time: new Date()}],
+        errors: []
+      }
+    
+    case UPLOAD_SUCCESS:
+      return {
+        file: action.file,
+        requesting: false,
+        successful: true,
+        progress: 100,
+        errors: [],
+        messages: [{
+          body: `Successfully save file.`,
+          time: new Date()
+        }]
+      }
+    
+    case UPLOAD_ERROR:
+      return {
+        file: state.file,
+        requesting: false,
+        successful: false,
+        progress: null,
+        errors: state.errors.concat([{
+          body: action.error.toString(),
+          time: new Date()
+        }]),
+        messages: []
+      }
+    
+    case UPLOAD_PROGRESS:
+      return {
+        file: action.file,
+        requesting: false,
+        successful: false,
+        progress: action.progress,
+        errors: [],
+        messages: []
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
